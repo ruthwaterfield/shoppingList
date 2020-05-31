16124c732cabf4b62b5b446a2efc245f4fcb9806
GolddolphinSKB-Fexcraftian <22482566+GolddolphinSKB-Fexcraftian@users.noreply.github.com>
2020-05-31 23:07:08 +0100

    Added more front end

--- shopping-list/src/List.js
+++ shopping-list/src/List.js
@@ -4,27 +4,40 @@ import './List.css';
 class List extends Component {
     constructor(props) {
         super(props)
-        this.state = {contents:["Carat", "Bean"]}
-        
+        this.state = {
+            contents:[],
+            addValue: ""
+        }
+        this.addItemToContents = this.addItemToContents.bind(this);
+        this.handleAddTextChange = this.handleAddTextChange.bind(this);
 
     }
 
-    addItemToContents(item) {
-        this.setState((state) => ({
-            contents: state.contents.concat(item)
-        }))
+    addItemToContents(event) {
+        // call api to save item
+        this.setState({contents: this.state.contents.concat(this.state.addValue)})
+        event.preventDefault()
+        this.setState({addValue: ""})
+    }
+
+    handleAddTextChange(event) {
+        this.setState({addValue: event.target.value})
     }
 
     render() {
         return (
         <div className="list">
+
             <h2>{this.props.type}</h2>
             <ul>
                 {this.state.contents.map(item => 
                     <li>{item}</li>
                 )}
             </ul>
-            <button onClick={this.addItemToContents()}>Add</button>
+            <form onSubmit={this.addItemToContents} className="addItemForm">
+                <input type="text" value={this.state.addValue} onChange={this.handleAddTextChange}/>
+                <input type="submit"/>
+            </form>
         </div>
     )}
 }
