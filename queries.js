153d55be52a738df78c8c5565751de93a1e9741b
Ruth Waterfield <ruth@gareth.lan>
2020-06-07 15:32:35 +0100

    Tidy up responses and headers

--- queries.js
+++ queries.js
@@ -2,11 +2,11 @@ const dbSettings = require('./dbSettings').dbSettings
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: dbSettings.user,
-    host: dbSettings.host,
-    database: dbSettings.database,
-    password: dbSettings.password,
-    port: dbSettings.port
+	user: dbSettings.user,
+	host: dbSettings.host,
+	database: dbSettings.database,
+	password: dbSettings.password,
+	port: dbSettings.port
 })
 
 const getPotentialItemsForSection = (request, response) => {
@@ -15,9 +15,16 @@ const getPotentialItemsForSection = (request, response) => {
 	pool.query('SELECT * FROM list_items WHERE section=$1;', [sectionId], function (error, results) {
 		if (error) {
 			console.log(error);
-			response.status(400)
+			response.status(400).send('Sorry there was a problem!')
+		} else {
+			// Website you wish to allow to connect
+			response.setHeader('Access-Control-Allow-Origin', '*');
+			// Request methods you wish to allow
+			response.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
+			response.status(200).json(results.rows)
 		}
-		response.status(200).json(results.rows)
+
 	})
 }
 
@@ -27,9 +34,15 @@ const getRequiredItemsForSection = (request, response) => {
 	pool.query('SELECT * FROM list_items WHERE section=$1 AND required=true;', [sectionId], function (error, results) {
 		if (error) {
 			console.log(error);
-			response.status(400)
+			response.status(400).send('Sorry there was a problem!')
+		} else {
+			// Website you wish to allow to connect
+			response.setHeader('Access-Control-Allow-Origin', '*');
+			// Request methods you wish to allow
+			response.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
+			response.status(200).json(results.rows)
 		}
-		response.status(200).json(results.rows)
 	})
 }
 
@@ -41,37 +54,65 @@ const getListItem = (request, response) => {
 		function (error, results) {
 			if (error) {
 				console.log(error);
-				response.status(400)
+				response.status(400).send('Sorry there was a problem!')
+			} else {
+				// Website you wish to allow to connect
+				response.setHeader('Access-Control-Allow-Origin', '*');
+				// Request methods you wish to allow
+				response.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
+				response.status(200).json(results.rows)
 			}
-			response.status(200).json(results.rows)
 		})
 }
 
 const addListItem = (request, response) => {
-	const { name, section, required, notes } = request.body
+	const {
+		name,
+		section,
+		required,
+		notes
+	} = request.body
 	pool.query('INSERT INTO list_items(name, section, required, notes) VALUES($1, $2, $3, $4);',
 		[name, section, required, notes],
 		function (error, result) {
 			if (error) {
 				console.log(error);
-				response.status(400)
+				response.status(400).send('Sorry there was a problem!')
+			} else {
+				// Website you wish to allow to connect
+				response.setHeader('Access-Control-Allow-Origin', '*');
+				// Request methods you wish to allow
+				response.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+
+				response.status(201).send(`User added with ID: ${result.insertId}`)
 			}
-			response.status(201).send(`User added with ID: ${result.insertId}`)
 		}
 	)
 }
 
 const editListItem = (request, response) => {
 	const itemId = request.params.itemId
-	const {name, section, required, notes } = request.body
+	const {
+		name,
+		section,
+		required,
+		notes
+	} = request.body
 	pool.query('UPDATE list_items SET name=$1, section=$2, required=$3, notes=$4 WHERE id=$5;',
 		[name, section, required, notes, itemId],
 		function (error) {
 			if (error) {
 				console.log(error);
-				response.status(400)
+				response.status(400).send('Sorry there was a problem!')
+			} else {
+				// Website you wish to allow to connect
+				response.setHeader('Access-Control-Allow-Origin', '*');
+				// Request methods you wish to allow
+				response.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+
+				response.status(200).send(`User modified with ID: ${itemId}`)
 			}
-			response.status(200).send(`User modified with ID: ${itemId}`)
 		}
 	)
 }
