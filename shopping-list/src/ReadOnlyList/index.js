c6d4c6c7ab586f688482fc598ec9e88607a57b13
Ruth Waterfield <ruth@gareth.lan>
2020-06-19 18:19:14 +0100

    Read only view styling

--- shopping-list/src/ReadOnlyList/index.js
+++ shopping-list/src/ReadOnlyList/index.js
@@ -37,13 +37,13 @@ class ReadOnlyList extends Component {
         return (
             <div className = "List-Holder">
                 {this.state.requiredItemsBySection.map(section => 
-                    <div className="bgColor m-2 p-2 position-relative justify-content-between text-left" key={section.sectionName}>
+                    <div className="bgColor m-2 p-2 position-relative justify-content-between text-left rounded" key={section.sectionName}>
                         <h2 className="m-2">{section.sectionName}</h2>
                         <Container className="flex-grow-1">
                             {section.requiredItems.map(item => 
-                            <Row className="itemColors d-flex justify-content-between m-1 p-0" key={item.id}>
+                            <Row className="itemColors d-flex justify-content-between m-1 p-0 rounded border" key={item.id}>
                                 <Col xs={1}><div className="w-75 h-75 border border-dark bg-white m-sm-1"/></Col>
-                                <Col xs={3}><strong>{item.name}</strong></Col>
+                                <Col xs={4}><strong>{item.name}</strong></Col>
                                 <Col>{item.notes}</Col>
                             </Row>
                             )}
